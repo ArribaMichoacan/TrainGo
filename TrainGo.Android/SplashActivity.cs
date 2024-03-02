2929ec0b84db90e21a46a2c67e90c8d0abdf4eeb
pedronAntrax <126541327+ArribaMichoacan@users.noreply.github.com>
2024-03-02 00:02:19 -0600

    Se agrego un splash screen

--- TrainGo.Android/SplashActivity.cs
+++ TrainGo.Android/SplashActivity.cs
@@ -8,10 +8,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace TrainGo.Droid
 {
-    public class SplashActivity
+    [Activity(Label = "TrainGo", Theme = "@style/SplashTheme",
+        MainLauncher = true, NoHistory = true, ConfigurationChanges = Android.Content.PM.ConfigChanges.ScreenSize)]
+    public class SplashActivity : Activity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            //iniciar actividad, desplegar splashScreen
+
+            //StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+
+            // Create your application here
+        }
+
+        protected override async void OnResume()
+        {
+            base.OnResume();
+            await SimulateStartUp();
+        }
+
+        private async Task SimulateStartUp()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+
     }
 }
