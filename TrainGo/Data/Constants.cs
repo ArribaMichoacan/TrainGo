18e402af703816a372eeb45af2273c6275760bff
pedronAntrax <126541327+ArribaMichoacan@users.noreply.github.com>
2024-03-01 23:42:24 -0600

    Agregar archivos de proyecto.

---
+++ TrainGo/Data/Constants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainGo.Data
+{
+    internal class Constants
+    {
+    }
+}
---
+++ TrainGo/Data/DataBaseHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainGo.Data
+{
+    public class DataBaseHelper
+    {
+    }
+}
